5142db494442664b6d8bb5e501621be76726534b
JosephRojas <josephrojas21@hotmail.com>
2019-09-29 17:53:40 -0500

    api rest create with crud

--- server/routes/employee.routes.js
+++ server/routes/employee.routes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res) => {
-    res.json({
-        status: 'Api works'
-    })
-})
+const employee = require('../controllers/employee.controller')
+
+
+router.get('/', employee.getEmployess);
+router.post('/', employee.createEmployee);
+router.get('/:id', employee.getEmployee);
+router.put('/:id', employee.editEmployee);
+router.delete('/:id', employee.deleteEmployee);
 
 module.exports = router;
